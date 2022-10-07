9a83d30ca22000bd56cf78fb86413f739432b802
Svyatoslav <sviatoslavnazin@gmail.com>
2022-10-07 10:50:16 +0800

    Refactor useFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,25 +2,18 @@ import { useState, useCallback } from "react";
 
 function useFetch<T extends any[]>(callback: (...args: T) => Promise<any>) {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
 
   const fetch: typeof callback = useCallback(async (...args: T) => {
     try {
-      setError(false);
       setLoading(true);
-      const res = await callback(...args);
-      if (res.status !== 200) {
-        throw new Error();
-      }
-    } catch {
-      setError(true);
+      await callback(...args);
     } finally {
       setLoading(false);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return { fetch, loading, error } as const;
+  return { fetch, loading } as const;
 }
 
 export default useFetch;
